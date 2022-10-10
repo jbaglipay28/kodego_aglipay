4ef82e01b697191b32fdf6734c07017cb9b4ff48
ENGINEER JEMSON <jbaglipay28@gmail.com>
2022-10-10 19:52:00 +0800

    updated Act_C.kt

--- src/main/kotlin/activity_01_a/Act_C.kt
+++ src/main/kotlin/activity_01_a/Act_C.kt
@@ -2,40 +2,39 @@ package activity_01_a
 
 class Act_C {
     var CleaningANDHygieneMaterial:String? =null
-        //toothpaste
-        //bathSoap
-        //Detergent
-        //Powdered soap
-        //Fabric Conditioner
-        //Shampoo
-        //BodyWash
-        //FacialWash
-        //Conditioner
-        //Dishwashing
+        val toothpaste = "is a key part of your daily oral hygiene routine. Along with your toothbrush and floss it helps to remove food debris and plaque from your teeth and gums."
+        val bathSoap = "A bar of soap cleanses the surface of your skin by dissolving the dirt layer. A body wash works in a similar way; however, it not only cleans the skin but also moisturizes it and addresses other skin concerns."
+        val Detergent = "A detergent is a substance or a mixture containing soaps and/or surfactants (any organic substance/mixture) intended for washing and cleaning processes."
+        val Fabric_Conditioner = " Fabric Conditioner protects your clothes. Downy strengthens fabrics and helps reduce friction during the laundry process, which helps clothes keep their original shape, reduce color fading, and reduce pilling and fuzz."
+        val Shampoo = "A shampoo should clean away the oil and dirt, rinse out easily, and leave your hair shiny, manageable, and flexible."
+        val BodyWash = "to cleanse your skin of impurities like oil, dirt, makeup, and sweat. Body wash is similar to bar soap and shower gel in that it creates lather and rids the body of dirt, but many differences set the three options apart."
+        val FacialWash = "face cleanser is made to purify, hydrate, and soothe your skin. The texture will be creamy, milky, gel-like, or even watery. Conversely, a face wash is made to clean your pores more deeply. The texture is often foamy or will lather into a foam when applied to your damp skin."
+        val Conditioner = "Conditioner is usually the second step to hair washing. While shampoo is formulated specifically to clean off sweat, dead skin cells, and hair products, conditioner makes hair softer and easier to manage. It also protects hair shafts from damage. "
+        val Dishwashing_Liquid = "Dishwashing liquid is used primarily for removing food from used dishes and tableware. Heavy soil (large food particles) is generally scraped from the dishes before using."
     var Condiments:String? = null
-        //Soy sauce
-        //Fish Sauce
-        //Cooking Oil
-        //Oyster Sauce
-        //Vinegar
+        val Soy_sauce = "Soy sauce can be added directly to food, and is used as a dip or salt flavor in cooking. It is often eaten with rice, noodles, and sushi or sashimi, or can also be mixed with ground wasabi for dipping. "
+        val Fish_Sauce = "Fish sauce, locally known in the Philippines as patis, is likewise a staple in Filipino households, used to add flavor and taste to Filipino soup dishes, seafood, meat and vegetable stew. "
+        val Cooking_Oil = "Cooking oil is plant, animal, or synthetic liquid fat used in frying, baking, and other types of cooking. It is also used in food preparation and flavoring not involving heat, such as salad dressings and bread dips, and may be called edible oil."
+        val Oyster_Sauce = "Oyster sauce is a sweet and salty condiment made primarily from oyster juices, salt, and sugar. It also boasts umami, which is a savory, tangy flavor. It's commonly used in Asian cuisines, including Chinese and Thai dishes, for stir-fries, meat marinades, and dipping sauces."
+        val Vinegar = " Vinegar is a combination of acetic acid and water. It is prepared by a two-step fermentation process of ethanol by bacteria. It is a colourless which has a pungent odour and sour taste."
+        val DryIngredients = "Ingredients are the things that are used to make something, especially all the different foods you use when you are cooking a particular dish."
     var Protein:String? = null
-        //Pork
-        //Beef
-        //Chicken
-        //Fish
-        //lamb
-        //Ham
-        //Seafood
+        val Pork = "Pork is meat from a pig, usually fresh and not smoked or salted."
+        val Beef = "the flesh of an adult domestic bovine (such as a steer or cow) used as food."
+        val Chicken = " a domestic fowl bred for its flesh or eggs, esp a young one. the flesh of such a bird used for food. any of various similar birds, such as a prairie chicken."
+        val Fish = "fish makes an essential contribution to a healthy diet. Its most important constituents include proteins, certain fatty acids, vitamins and minerals."
+        val lamb = "Lamb is the meat of young domestic sheep (Ovis aries). It's a type of red meat — a term used for the meat of mammals that is richer in iron than chicken or fish. The meat of young sheep — in their first year — is known as lamb, whereas mutton is a term used for the meat of adult sheep."
+        val Seafood = "seafood, edible aquatic animals, excluding mammals, but including both freshwater and ocean creatures. Most nontoxic aquatic species are exploited for food by humans. Even those with toxic properties, such as certain blowfish, can be prepared so as to circumvent harm to the consumer."
     var ProcessedFoodandDrinks:String? =null
-        //FrozenFoods
-        //Cereal
-        //CannedGoods
-        //Junkfoods
-        //Liqour
-        //Soda/Softdrinks
-        //Juice
-        //Noodless
-        //Milk
+        val FrozenFoods = "food that has been subjected to rapid freezing and is kept frozen until used."
+        val Cereal = "Cereals are the edible seeds or grains of the Gramineae grass family. Cereals grown in various countries include rye, oats, barley, maize, triticale, millet, and sorghum. Many consider cereals as staple foods that are high in nutrients in both developed and developing countries."
+        val CannedGoods = "food preserved by canning. canned food, canned foods, tinned goods. food product, foodstuff - a substance that can be used or prepared for use as food. canned meat, tinned meat - meat preserved in a can or tin."
+        val Junkfoods = "is a term used to describe food that is high in calories from sugar and/or fat, and possibly also sodium, but with little dietary fiber, protein, vitamins, minerals, or other important forms of nutritional value."
+        val Liqour = "Liquor, also known as spirits are alcoholic beverages made of plants or grains and concocted into a potent drink. Simply put, staples like gin, vodka, rum and tequila are all forms of liquor. Liquor also is distilled and has an alcohol content of at least 20 percent, although 40 percent is more common."
+        val Softdrinks = "soft drink, any of a class of nonalcoholic beverages, usually but not necessarily carbonated, normally containing a natural or artificial sweetening agent, edible acids, natural or artificial flavours, and sometimes juice. Natural flavours are derived from fruits, nuts, berries, roots, herbs, and other plant sources. Coffee, tea, milk, cocoa, and undiluted fruit and vegetable juices are not considered soft drinks."
+        val Juice = "Juice is a drink made from the extraction or pressing of the natural liquid contained in fruit and vegetables. It can also refer to liquids that are flavored with concentrate or other biological food sources, such as meat or seafood, such as clam juice. Juice is commonly consumed as a beverage or used as an ingredient or flavoring in foods or other beverages, as for smoothies."
+        val Noodless = "Noodles are a type of food made from unleavened dough which is rolled flat and cut, stretched or extruded, into long strips or strings. Noodles are a staple food in many cultures (see Chinese noodles, Filipino noodles, Indonesian noodles, Japanese noodles, Korean noodles, Vietnamese noodles, and Italian pasta) made from unleavened dough which is stretched, extruded, or rolled flat and cut into one of a variety of shapes."
+        val Milk = "Milk is a white liquid food produced by the mammary glands of mammals. It is the primary source of nutrition for young mammals (including breastfed human infants) before they are able to digest solid food."
 
 
 
