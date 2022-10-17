1c37b9d722cb0970b339ed7e57384513fcbd6b43
ENGINEER JEMSON <jbaglipay28@gmail.com>
2022-10-17 18:33:32 +0800

    updated Act_D.kt

--- src/main/kotlin/activity_01/Act_D.kt
+++ src/main/kotlin/activity_01/Act_D.kt
@@ -1,30 +1,83 @@
 package activity_01
 
-fun main(){
-    print("Enter 5 monetary amount : \n")
-    var cash1:Int? = null
-    cash1 = readln().toInt()
-    var cash2:Int? = null
-    cash2 = readln().toInt()
-    var cash3:Int? = null
-    cash3 = readln().toInt()
-    var cash4:Int? = null
-    cash4 = readln().toInt()
-    var cash5:Int? = null
-    cash5 = readln().toInt()
+import mu.KotlinLogging
+import java.util.logging.Logger
+
+private val logger = KotlinLogging.logger{}
+fun main() {
 
 
 
-    var total = cash1 + cash2 + cash3 + cash4 + cash5
-    println("Total of the 5 monetary value: $total")
+    logger.info { "INPUT 1st MONETARY VALUE " }
+    var cash1 = readln().toIntOrNull()
+    if (cash1 == null) {
 
-    println("Divide the value by how many? : ")
-    var divisor = 0.00f
-    divisor = readln().toFloat()
+        logger.info { "invalid input " }
+        cash1 = readln().toIntOrNull()
+    }else  if (cash1 != null){
+           logger.info { "INPUT 2nd MONETARY VALUE " }
+           var cash2= readln().toIntOrNull()
 
-    println("RESULT : ${total/divisor}")
-
-
-
-
+           if( cash2 == null) {
+               logger.info { "invalid input " }
+           } else if (cash2!=null){
+               logger.info { "INPUT 3rd MONETARY VALUE " }
+               var cash3= readln().toIntOrNull()
+               if (cash3==null){
+                   logger.info { "invalid input " }
+               }else if (cash3!=null){
+                   logger.info { "INPUT 4th MONETARY VALUE " }
+                   var cash4= readln().toIntOrNull()
+                   if (cash4 == null){
+                       logger.info { "invalid input " }
+                   }
+                   else if (cash4 != null ) {
+                       logger.info { "INPUT 5th MONETARY VALUE " }
+                       var cash5= readln().toIntOrNull()
+                       if (cash5==null){
+                           logger.info { "invalid input " }
+                       }else if (cash5!=null){
+                           var total = cash1.toInt() + cash2.toInt() + cash3.toInt() + cash4.toInt() + cash5.toInt()
+                            logger.info { "Total of the 5 monetary value :$total" }
+                            var divisor:String?=null
+                            logger.info { "Divide the value by how many? : " }
+                            divisor = readln().toString()
+                            var newDivisor = divisor.toFloat()
+                            logger.info { "RESULT : ${total/newDivisor} " }
+                       }
+                   }
+              }
+           }
+        }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
